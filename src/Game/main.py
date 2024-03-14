6a335654d8bf324d52caab74aa6ef690f70678a9
Long Bùi <144930888+GDevNeON@users.noreply.github.com>
2024-03-14 16:02:58 +0700

    Update main.py

--- src/Game/main.py
+++ src/Game/main.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 from pygame.locals import (
+    RLEACCEL,
     K_w,
     K_a, 
     K_s, 
@@ -9,11 +10,18 @@ from pygame.locals import (
     KEYDOWN,
     QUIT,
 )
+
+#Init sounds, pygame
+pygame.mixer.init()
 pygame.init()
+
+#Framerate per second
+FPS = 30
 
 #Screen resolution
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
+
 #Basic color code
 Black = (0,0,0)
 White = (255,255,255)
@@ -31,6 +39,7 @@ Green = (0,128,0)
 Purple = (128,0,128)
 Teal = (0,128,128)
 Navy = (0,0,128)
+
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -69,24 +78,40 @@ class Enemy(pygame.sprite.Sprite):
         self.surf = pygame.Surface((20, 10))
         self.surf.fill((255, 255, 255))
         self.rect = self.surf.get_rect(
-            center=(
+            center = (
                 random.randint(SCREEN_WIDTH + 20, SCREEN_WIDTH + 100),
                 random.randint(0, SCREEN_HEIGHT),
             )
         )
         self.speed = random.randint(5, 20)
-
+        
     # Move the sprite based on speed
     # Remove the sprite when it passes the left edge of the screen
     def update(self):
         self.rect.move_ip(-self.speed, 0)
         if self.rect.right < 0:
             self.kill()
-        
 
+
+clock = pygame.time.Clock()
+
+# Create a screen and set window's name
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.RESIZABLE)
+screen.set_title('A 2D NORMAL SHOOTING GAME')
 
+# Create custom events
+ADDENEMY = pygame.USEREVENT + 1
+pygame.time.set_timer(ADDENEMY, 250)
+
+# Create the 'player'
 player = Player()
+
+# Create groups to hold enemy sprites and all sprites
+# - enemies is used for collision detection and position updates
+# - all_sprites is used for rendering
+enemies = pygame.sprite.Group()
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
 
 running = True
 while running:
@@ -98,16 +123,32 @@ while running:
         if event.type == KEYDOWN:
             if event.key == K_ESCAPE:
                 running = False
-
+                
         elif event.type == QUIT:
             running = False
-        
-    
-    player.update(pressed_keys)
             
+         # Add a new enemy?
+        elif event.type == ADDENEMY:
+            # Create the new enemy and add it to sprite groups
+            new_enemy = Enemy()
+            enemies.add(new_enemy)
+            all_sprites.add(new_enemy)
+            
+    player.update(pressed_keys)
+    enemies.update()
+    
+    for entity in all_sprites:
+        screen.blit(entity.surf, entity.rect)        
     screen.blit(player.surf, player.rect)
+    
+    # Check if any enemies have collided with the player
+    if pygame.sprite.spritecollideany(player, enemies):
+        # If so, then remove the player and stop the loop
+        player.kill()
+        running = False
     
     pygame.display.update()
     
+    clock.tick(FPS)
     
 pygame.quit()  
